003da6df41d9964ed6718a1ad516d1cb03d6bb04
elReclusa <howell_j@hotmail.com>
2022-10-09 15:35:39 -0500

    Started pseudocode, added some actual code

---
+++ JS/script.js
@@ -0,0 +1,30 @@
+// gonna need some variables here:
+
+// var timerEl = document.querySelector(".timer-count");
+// var startButton = document.querySelector(".start-button");
+//vars for correct and incorrect?
+
+// init function when page loads
+
+//startQuiz function called when start button is clicked
+
+//setTimer function starts and stops the timer and calls score results when timer=0
+
+//functions used by init for high score storage and display
+// tests if button clicked is correct answer
+// counts number of correct answers for score
+
+//event listener for clicks on buttons
+
+//event listener 
+
+//test if button clicked is correct answer
+
+//event listener for start button to call startQuiz function
+// document.addEventListener("click", function(event) {
+    // If the count is zero, exit function
+ //   if (timerCount === 0) {
+  //    return;
+  //  }
+//call init() so it runs when page is opened
+//init(); 
