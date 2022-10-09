8763c294f7d287ac31c8ecda01c6228f1deeafc9
elReclusa <howell_j@hotmail.com>
2022-10-09 16:54:02 -0500

    Added some JS code and coomments

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,13 @@
 // gonna need some variables here:
+var questions= [
+    {
+        question: "question",
+        choices: ["a","b","c","d"],
+        answer: "a"
+    }
+]
+var questionIndex = 0; 
+
 
 // var timerEl = document.querySelector(".timer-count");
 // var startButton = document.querySelector(".start-button");
@@ -11,14 +20,18 @@
 //setTimer function starts and stops the timer and calls score results when timer=0
 
 //functions used by init for high score storage and display
+
 // tests if button clicked is correct answer
+
+//var [questionIndex]
+
 // counts number of correct answers for score
 
 //event listener for clicks on buttons
 
 //event listener 
 
-//test if button clicked is correct answer
+//test if button clicked is correct answer, add point if correct, add zero if not
 
 //event listener for start button to call startQuiz function
 // document.addEventListener("click", function(event) {
@@ -26,5 +39,6 @@
  //   if (timerCount === 0) {
   //    return;
   //  }
-//call init() so it runs when page is opened
+
+  //call init() so it runs when page is opened
 //init(); 
