e4654a4aec8ec53a959c1875698fff28de9a56b8
elReclusa <howell_j@hotmail.com>
2022-10-10 17:59:22 -0500

    Added a ton of code, mostly works

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,98 @@
 // gonna need some variables here:
-var questions= [
+var questions = [
+
+
     {
         question: "question",
-        choices: ["a","b","c","d"],
+        choices: ["a", "b", "c", "d"],
+        answer: "a"
+    },
+
+    {
+        question: "question 2",
+        choices: ["a", "b", "c", "d"],
+        answer: "a"
+    },
+
+    {
+        question: "question 3",
+        choices: ["a", "b", "c", "d"],
+        answer: "a"
+    },
+
+    {
+        question: "question 4",
+        choices: ["a", "b", "c", "d"],
+        answer: "a"
+    },
+    {
+        question: "question 5",
+        choices: ["a", "b", "c", "d"],
+        answer: "a"
+    },
+    {
+        question: "question 6",
+        choices: ["a", "b", "c", "d"],
+        answer: "a"
+    },
+    {
+        question: "question 7",
+        choices: ["a", "b", "c", "d"],
         answer: "a"
     }
 ]
-var questionIndex = 0; 
+var questionIndex = 0;
 
+var startArea = document.querySelector(".start");
+var startButton = document.querySelector(".startButton");
+var quizArea = document.querySelector(".quiz");
+startButton.addEventListener("click", function () {
+    startArea.style.display = "none"
+    quizArea.style.display = "block"
+    displayQuestion()
+    quizTimer()
+})
+function displayQuestion() {
+    if (questionIndex === questions.length){
+        quizArea.style.display = "none"
+        clearInterval(timer)
+        return
+    }
+    document.querySelector(".questionText").innerText = questions[questionIndex].question
+    document.querySelector(".button1").innerText = questions[questionIndex].choices[0]
+    document.querySelector(".button2").innerText = questions[questionIndex].choices[1]
+    document.querySelector(".button3").innerText = questions[questionIndex].choices[2]
+    document.querySelector(".button4").innerText = questions[questionIndex].choices[3]
+}
 
+function answerQuestion(event){
+    var choice=event.target.innerText
+    var correct=questions[questionIndex].answer
+    if (choice === correct){
+        questionIndex++
+        displayQuestion()
+    } else {
+        questionIndex++
+        time= time-10
+        displayQuestion()
+    }
+}
+
+var timer
+var time =60
+function quizTimer(){
+    timer=setInterval(function(){
+        if (time >0){
+            time=time-1
+            document.querySelector(".time").innerText = time
+        }
+    },1000)
+}
+
+document.querySelector(".button1").addEventListener("click", answerQuestion)
+document.querySelector(".button2").addEventListener("click", answerQuestion)
+document.querySelector(".button3").addEventListener("click", answerQuestion)
+document.querySelector(".button4").addEventListener("click", answerQuestion)
 // var timerEl = document.querySelector(".timer-count");
 // var startButton = document.querySelector(".start-button");
 //vars for correct and incorrect?
